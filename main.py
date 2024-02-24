06a3294934809bedc27cabcd2381224673afb8a1
NePupsik <saanma300@gmail.com>
2024-02-24 11:10:16 +0300

    main 2

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ import requests
 import lxml
 from bs4 import BeautifulSoup
 
-url = "https://kups.club/"
-
 
 session = requests.Session()
 header = {"user agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"}
 
-response = session.get(url, headers=header)
-soup = BeautifulSoup(response.text, "lxml")
+for j in range(1, 7):
+    url = f"https://cash-backer.club/shops?page={j}"
 
-all_product = soup.find("div", class_ ="row mt-4")
+    response = session.get(url, headers=header)
+    soup = BeautifulSoup(response.text, "lxml")
 
-products = all_product.find_all("div", class_="col-lg-4 col-md-4 col-sm-6 portfolio-item ")
+    all_product = soup.find("div", class_="col-lg-2 col-md-3 shop-list-card pseudo-link no-link")
+
+    products = all_product.find_all("div", class_="row col-lg-9 col-md-9 col-12")
+
+    for elem in products:
+        price = elem.find("p", class_="card-text").text
+        price = price[price.find(":")+1:price.find("%")]
+        title = elem.find("h3", class_="card-title").text.strip()
+        with open("kups.txt", "a", encoding="utf-8") as file:
+            file.write(f"{title} ---->>>>{price}\n")
