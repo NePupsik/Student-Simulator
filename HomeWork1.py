58699a387e032a46a82b24e295ffa38638f46912
NePupsik <saanma300@gmail.com>
2024-02-24 10:03:49 +0300

    HomeWork2.0

--- HomeWork1.py
+++ HomeWork1.py
@@ -1,19 +1,29 @@
 class Animal:
-    def __init__(self):
-        name = "name"
-        species = "species"
-        legs = "legs"
+    def __init__(self, name, species, legs):
+        self.name = name
+        self.species = species
+        self.legs = legs
     def voice(self):
-        pass
+        print(f"{self.name} is speaking")
     def move(self):
-        pass
+        print(f"{self.species} is moving")
 class Dog(Animal):
-    def __init__(self, breed, name, species, legs, bark):
-        breed = "breed"
+    def __init__(self, breed, name, species, legs):
+        super().__init__(breed, name, species, legs)
+        self.breed = "breed"
     def bark(self):
-        pass
+        print(f"{self.name}{self.species} is barking, his breed is {self.breed}")
 class Bird(Animal):
-    def __init__(self, wingspan, name, species, legs, fly):
-        wingspan = "wingspan"
+    def __init__(self, wingspan, name, species, legs):
+        super().__init__(wingspan, name, species, legs)
+        self.wingspan = "wingspan"
     def fly(self):
-        pass
+        print(f"{self.name}{self.species} is flying, wingspan = {self.wingspan}")
+
+bird = Bird(wingspan = 185, name = "Lebed", species = "Swan", legs = 2)
+print(bird)
+bird.fly()
+
+dog = Dog(breed="doggy", name = "Bobik", species = "Barker", legs=4)
+print(dog)
+dog.bark()
